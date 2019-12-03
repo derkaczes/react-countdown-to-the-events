f15cd946e4b8a4aa5f2c73de1e27772262a7ec62
Michał Derkacz <derkaczes@gmail.com>
2019-12-03 15:52:23 +0100

    Add key for component

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ class App extends Component {
     }
     render() {
         const events = this.state.events.map(el => {
-            return <Countdown name={el.name} time={el.time}/>
+            return <Countdown key={el.id} name={el.name} time={el.time}/>
         })
         return (
             <div>{events}</div>
