edef5c95805a3ec81fdd7218423588ca35b2be60
Michał Derkacz <derkaczes@gmail.com>
2019-12-03 15:49:11 +0100

    Create and generate new Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Countdown from './CountDown';
 
-const App = () => (
-<div>
-    <Countdown name="śniadanie" time="7:00"/>
-</div>
-);
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            events: [
+                { id: 0, name: "śniadanie", time: "7:00" },
+                { id: 1, name: "obiad", time: "16:00" }
+            ]
+        }
+    }
+    render() {
+        const events = this.state.events.map(el => {
+            return <Countdown name={el.name} time={el.time}/>
+        })
+        return (
+            <div>{events}</div>
+        );
+    }
+}
 
 export default App;
