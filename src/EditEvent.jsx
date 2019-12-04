6013d12494a690590763a2ab9030b0650b10eec1
Michał Derkacz <derkaczes@gmail.com>
2019-12-04 15:27:21 +0100

    Adding form validation

--- src/EditEvent.jsx
+++ src/EditEvent.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
+import { isValidNumberInput } from "./utils";
 
 const EditEvent = props => {
     return (
         <div className="edit-event">
             <div className="edit-event__input-group">
                 <label htmlFor="name">name </label>
-                <input type="text" id="name" name="name" value={props.name} onChange={e => props.onInputChange({ [e.target.name]: e.target.value})}/>
+                <input 
+                    type="text" 
+                    id="name" 
+                    name="name" 
+                    value={props.name} 
+                    onChange={e => props.onInputChange({ [e.target.name]: e.target.value})}
+                />
             </div>
             <div className="edit-event__input-group">
                 <label htmlFor="hour">hour </label>
-                <input type="tel" id="hour" name="hour" value={props.hour} onChange={e => props.onInputChange({ [e.target.name]: e.target.value})}/>
+                <input 
+                    type="tel" 
+                    id="hour" 
+                    name="hour" 
+                    value={props.hour} 
+                    onKeyPress={e => isValidNumberInput(e)} 
+                    onChange={e => props.onInputChange({ [e.target.name]: e.target.value})}
+                />
             </div>
             <div className="edit-event__input-group">
                 <label htmlFor="minute">minute </label>
-                <input type="tel" id="minute" name="minute" value={props.minute} onChange={e => props.onInputChange({ [e.target.name]: e.target.value})}/>
+                <input 
+                    type="tel" 
+                    id="minute" 
+                    name="minute" 
+                    value={props.minute}
+                    onKeyPress={e => isValidNumberInput(e)}  
+                    onChange={e => props.onInputChange({ [e.target.name]: e.target.value})}
+                />
             </div>
             <button onClick={() => props.onSave()}>Add</button>
             <button>Cancel</button>
